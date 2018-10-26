15b143e483ddfbe28b8b16c982155ab2b690162f
Atishya Jain <atishya.jain@rocketmail.com>
2018-10-26 17:02:21 +0530

    Some changes

--- src/FBverification.js
+++ src/FBverification.js
@@ -1,10 +1,52 @@
 module.exports = (req, res) => {
-	const hubChallenge = req.query['hub.challenge'];
-	const hubMode = req.query['hub.mode'];
-	const verifyTokenMatches = (req.query['hub.verify_token'] === 'I_am_too_tired_to_work_any_longer');
+	var tmp = req._url.query;
+	tmp = tmp.split('&');
+	const hubChallenge = tmp[1].split('=')[1];
+	const hubMode = tmp[0].split('=')[1];
+	const verifyTokenMatches = (tmp[2].split('=')[1] === 'I_am_too_tired_to_work_any_longer');
+	// console.log(req)
+	console.log(hubMode && verifyTokenMatches)
+
 	if (hubMode && verifyTokenMatches) {
-		res.status(200).send(hubChallenge);
+		// res.status(200).send(hubChallenge
+		// console.log(parseInt(hubChallenge));
+		// res.send(parseInt(hubChallenge));
+
+		res.send(200, parseInt(hubChallenge));
+
+
+	    // res.status(200).json({
+	      // 'error': false,
+	      // 'message': hubChallenge,
+	      // 'data': data
+	    // });
+
+
+
+
 	} else {
-		res.status(403).end();
+		// console.log(req.query['hub.challenge']);
+		res.status(403);
 	}
 };
+
+// hub.mode=subscribe&hub.challenge=171344716&hub.verify_token=I_am_too_tired_to_work_any_longer
+// hub.mode=subscribe&hub.challenge=598277699&hub.verify_token=I_am_too_tired_to_work_any_longer
+
+
+// module.exports = (req, res) => {
+// 	var tmp = req._url.query;
+// 	tmp = tmp.split('&');
+// 	const hubChallenge = req.query['hub.challenge'];
+// 	const hubMode = req.query['hub.mode'];
+// 	const verifyTokenMatches = (req.query['hub.verify_token'] === 'I_am_too_tired_to_work_any_longer');
+// 	// console.log(req)
+// 	console.log(req._url.query)
+
+// 	if (hubMode && verifyTokenMatches) {
+// 		res.status(200).send(hubChallenge);
+// 	} else {
+// 		// console.log(req.query['hub.challenge']);
+// 		res.status(403).end();
+// 	}
+// };
